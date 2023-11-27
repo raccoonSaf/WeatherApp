2c5ae3a923a0beabeab273367e76d7259848a6eb
Admin <raccoonsaf@rambler.ru>
2023-11-27 17:24:28 +0300

    first

---
+++ app/src/main/java/pl/alexbul/weatherapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package pl.alexbul.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pl.alexbul.weatherapp.screens.MainScreen
+import pl.alexbul.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+              MainScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    WeatherAppTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/pl/alexbul/weatherapp/screens/MainScreen.kt
@@ -0,0 +1,135 @@
+package pl.alexbul.weatherapp.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import pl.alexbul.weatherapp.R
+import pl.alexbul.weatherapp.ui.theme.BlueLight
+
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreen() {
+    Image(
+        painter = painterResource(id = R.drawable.fon), contentDescription = "fon1",
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.8f),
+        contentScale = ContentScale.FillBounds
+    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(5.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth(),
+            colors = CardDefaults.cardColors(
+                containerColor = BlueLight, //Card background color
+                // contentColor = Color.White  //Card content color,e.g.text
+            ),
+            shape = RoundedCornerShape(10.dp)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+
+                )
+
+            {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp, start = 10.dp),
+                        text = "20 Jun 2022 13:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    AsyncImage(
+                        model = "https://cdn.weatherapi.com/weather/64x64/day/296.png",
+                        contentDescription = "im1",
+                        modifier = Modifier
+                            .padding(top = 2.dp, end = 8.dp)
+                            .size(37.dp)
+                    )
+
+                }
+
+                Text(
+                    text = "London",
+                    style = TextStyle(fontSize = 25.sp),
+                    color = Color.White
+                )
+
+                Text(
+                    text = "67 Pp",
+                    style = TextStyle(fontSize = 55.sp),
+                    color = Color.White
+                )
+
+                Text(
+                    text = "cloudy",
+                    style = TextStyle(fontSize = 20.sp),
+                    color = Color.White
+                )
+
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                            Icon(painter = painterResource(id = R.drawable.ic_search),
+                                contentDescription = "iconSearch" )
+
+                    }
+                    Text(
+                        modifier = Modifier.padding(top = 10.dp, start = 10.dp),
+                        text = "20 Jun 2022 13:00",
+                        style = TextStyle(fontSize = 15.sp),
+                        color = Color.White
+                    )
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(painter = painterResource(id = R.drawable.ic_synck),
+                            contentDescription = "iconSync" )
+
+                    }
+
+                }
+
+
+            }
+        }
+
+    }
+}
