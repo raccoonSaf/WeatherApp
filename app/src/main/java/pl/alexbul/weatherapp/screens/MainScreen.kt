e35179c9539780943e4d299c13d5def894a4c906
Raccoon <gygel@tut.by>
2023-11-29 22:08:30 +0300

    added TabLayout and new dependencies

--- app/src/main/java/pl/alexbul/weatherapp/screens/MainScreen.kt
+++ app/src/main/java/pl/alexbul/weatherapp/screens/MainScreen.kt
@@ -1,10 +1,9 @@
 package pl.alexbul.weatherapp.screens
 
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,40 +12,41 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Tab
+import androidx.compose.material.TabRow
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.pagerTabIndicatorOffset
+import com.google.accompanist.pager.rememberPagerState
+import kotlinx.coroutines.launch
 import pl.alexbul.weatherapp.R
 import pl.alexbul.weatherapp.ui.theme.BlueLight
 
 
 @Preview(showBackground = true)
 @Composable
-fun MainScreen() {
+fun MainCard() {
 
     //comment for second dev commit
 
-    Image(
-        painter = painterResource(id = R.drawable.fon), contentDescription = "fon1",
-        modifier = Modifier
-            .fillMaxSize()
-            .alpha(0.8f),
-        contentScale = ContentScale.FillBounds
-    )
 
     Column(
         modifier = Modifier
-            .fillMaxSize()
+
             .padding(5.dp)
     ) {
         Card(
@@ -112,8 +112,10 @@ fun MainScreen() {
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
                     IconButton(onClick = { /*TODO*/ }) {
-                            Icon(painter = painterResource(id = R.drawable.ic_search),
-                                contentDescription = "iconSearch" )
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_search),
+                            contentDescription = "iconSearch"
+                        )
 
                     }
                     Text(
@@ -123,8 +125,10 @@ fun MainScreen() {
                         color = Color.White
                     )
                     IconButton(onClick = { /*TODO*/ }) {
-                        Icon(painter = painterResource(id = R.drawable.ic_synck),
-                            contentDescription = "iconSync" )
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_synck),
+                            contentDescription = "iconSync"
+                        )
 
                     }
 
@@ -135,4 +139,50 @@ fun MainScreen() {
         }
 
     }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun TabLayout() {
+    val tabList = listOf("HOURS", "DAYS")
+    val pagerState = rememberPagerState()
+    val tabIndex = pagerState.currentPage
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier
+        .padding(5.dp)
+        .clip(RoundedCornerShape(5.dp)))
+    {
+        TabRow(
+            selectedTabIndex = tabIndex, indicator = { post ->
+                TabRowDefaults.Indicator(
+                    Modifier.pagerTabIndicatorOffset(pagerState,post))
+
+
+            },
+         backgroundColor = BlueLight
+        ) {
+            tabList.forEachIndexed { index, text ->
+                Tab(
+                    selected = false,
+                    onClick = {
+                              coroutineScope.launch {
+                                  pagerState.animateScrollToPage(index)
+                              }
+
+                    },
+                    text = {
+                        Text(text = text)
+                    }
+
+                )
+
+            }
+        }
+        HorizontalPager(count = tabList.size, state =pagerState,
+            modifier = Modifier.weight(1.0f)) {
+            index ->
+        }
+    }
+    
 }
